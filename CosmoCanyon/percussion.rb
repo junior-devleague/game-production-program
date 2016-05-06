c30b4b38bc028158729ee34c95e8a47e6ec5ed19
Aisis <hi@aisis.xyz>
2016-05-06 06:47:59 -1000

    Added files via upload

---
+++ CosmoCanyon/percussion.rb
@@ -0,0 +1,13 @@
+#Percussion 1 drum roll 116 
+A0 0.125
+#Percussion 2 116 ticking sound 4times per measure
+4.times
+F0 0.125
+F0 0.0625
+F0 0.0625
+#SUZU socal sounds like jingles
+# 1-30
+Fs2 0.25 
+Fs2 0.25
+Fs2 0.25
+Fs2 0.25
