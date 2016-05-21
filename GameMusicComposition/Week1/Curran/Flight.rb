6ac821810185222e0a100885a24de2b8e8d19a38
Curran808 <curranmatsuki@student.midpac.edu>
2016-05-21 10:21:41 -1000

    Create Flight.rb

---
+++ GameMusicComposition/Week1/Curran/Flight.rb
@@ -0,0 +1,19 @@
+# Welcome to Sonic Pi v2.10
+use_bpm 57
+use_synth :piano
+play :fs4, release: 1.25
+play :fs3, release: 3.75
+sleep 0.1
+play :cs5, release: 0.0625
+sleep 0.125
+play :b4, release: 0.0625
+sleep 0.1
+play :e5, release: 0.125
+sleep 0.1
+play :a2, release: 0.250
+sleep 0.1
+play :a4, release: 0.0625
+sleep 0.1
+play :gs4, release: 0.0625
+sleep 0.1
+play :fs4, release: 0.125
