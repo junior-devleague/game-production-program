69e0fee860bca2bdf30642f8044086be373514f8
MEEP <ianktalbert@me.com>
2016-05-21 12:05:45 -1000

    music dun
    making rhythm game moosic

---
+++ GameMusicComposition/Week3/Ian/stuff.rb
@@ -0,0 +1,15 @@
+4.times do
+  use_synth :blade
+  use_bpm 120
+  play:Gs4, release: 1
+  sleep 1
+  play:G5, release: 0.125
+  sleep 1
+  play:G5, release: 0.125
+  play:Gs4, release: 1
+  sleep 1
+end
+
+
+
+
