c27728a8a5cc1571c382cae7414e7f4975ea6403
MEEP <ianktalbert@me.com>
2016-05-21 10:29:33 -1000

    Added music I made
    @aisis I made meepmusic 1 and 2 and added harmony

---
+++ GameMusicComposition/Week3/Ian/meepmusic2.rb
@@ -0,0 +1,33 @@
+use_synth :piano
+5.times do
+  2.times do
+    play :G5, release: 2.5
+    sleep 2.3
+    play :Gs5, release: 0.75
+    sleep 0.75
+    play :G4, release: 2.5
+    sleep 2.3
+    play :gs4, release: 0.75
+    sleep 0.75
+    play :Gs3, release: 2.5
+    sleep 2.3
+    play :A4, release: 0.75
+    sleep 0.75
+    play :As4, release: 2.5
+    sleep 2.3
+    play :A4, release: 0.75
+    sleep 0.75
+    play :As4, release: 2.5
+  end
+  play :G5, release: 2.5
+  use_synth :tri
+  2.times do
+    play :As4, release: 2.5
+    sleep 2.3
+    play :A4, release: 0.125
+    sleep 0.2
+    play :A4, release: 0.125
+    sleep 0.7
+    play :As4, release: 2.5
+  end
+end
