44cacc63df5b4d5ae271eedf082c6ce01b374781
Aisis <aisis@users.noreply.github.com>
2016-05-22 18:46:04 -1000

    13 intervals in one line
    Here is how you can define your musical code block to a function.

---
+++ GameMusicComposition/Week3/Aisis/13intervals-fn.rb
@@ -0,0 +1,83 @@
+define :unison do
+  play 60
+  play 60
+  sleep 1
+end
+define :min2 do
+  play 60
+  play 61
+  sleep 1
+end
+define :maj2 do
+  play 60
+  play 62
+  sleep 1
+end
+define :min3 do
+  play 60
+  play 63
+  sleep 1
+end
+define :maj3 do
+  play 60
+  play 64
+  sleep 1
+end
+define :perfect4 do
+  play 60
+  play 65
+  sleep 1
+end
+define :aug4 do
+  play 60
+  play 66
+  sleep 1
+end
+define :perfect5 do
+  play 60
+  play 67
+  sleep 1
+end
+define :min6 do
+  play 60
+  play 68
+  sleep 1
+end
+define :maj6 do
+  play 60
+  play 69
+  sleep 1
+end
+define :min7 do
+  play 60
+  play 70
+  sleep 1
+end
+define :maj7 do
+  play 60
+  play 71
+  sleep 1
+end
+define :octave do
+  play 60
+  play 72
+  sleep 1
+end
+
+
+unison
+min2
+maj2
+min3
+maj3
+perfect4
+aug4
+perfect5
+min6
+maj6
+min7
+maj7
+octave
+
+
+
