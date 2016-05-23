6f6b25ba9c0242a547f05fece8b1cae474285f83
Aisis <aisis@users.noreply.github.com>
2016-05-22 18:47:45 -1000

    13 intervals in SP

---
+++ GameMusicComposition/Week3/Aisis/13intervals.rb
@@ -0,0 +1,52 @@
+##| unison/prime
+play :C4
+play :C4
+sleep 1
+##| minor second/m2
+play :C4
+play :Cs4
+sleep 1
+##| major second /M2
+play :C4
+play :D4
+sleep 1
+##| minor third /m3
+play :C4
+play :Ds4
+sleep 1
+##| major third/ M3
+play :C4
+play :E4
+sleep 1
+##| perfect fourth /P4
+play :C4
+play :F4
+sleep 1
+##| augmented fourth/ A4 (interval)
+play :C4
+play :Fs4
+sleep 1
+##| perfect fifth
+play :C4
+play :G4
+sleep 1
+##| minor sixth / m6
+play :C4
+play :Gs4
+sleep 1
+##| Major sixth / M6
+play :C4
+play :A4
+sleep 1
+##| minor seventh / m7
+play :C4
+play :As4
+sleep 1
+##| Major seventh / M7
+play :C4
+play :B4
+sleep 1
+##| Octave 
+play :C4
+play :C5
+sleep 1
