15d35c4ad8eb2c344043aa24a64cd8580c88790e
Aisis <aisis@users.noreply.github.com>
2016-05-23 02:01:53 -1000

    Create variation13intervalsfn.rb

---
+++ GameMusicComposition/Week3/Aisis/variation13intervalsfn.rb
@@ -0,0 +1,79 @@
+define :unison do
+  play 60
+  play 60
+end
+define :min2 do
+  play 60
+  play 61
+end
+define :maj2 do
+  play 60
+  play 62
+end
+define :min3 do
+  play 60
+  play 63
+end
+define :maj3 do
+  play 60
+  play 64
+end
+define :perfect4 do
+  play 60
+  play 65
+end
+define :aug4 do
+  play 60
+  play 66
+end
+define :perfect5 do
+  play 60
+  play 67
+end
+define :min6 do
+  play 60
+  play 68
+end
+define :maj6 do
+  play 60
+  play 69
+end
+define :min7 do
+  play 60
+  play 70
+end
+define :maj7 do
+  play 60
+  play 71
+end
+define :octave do
+  play 60
+  play 72
+end
+
+unison
+sleep 1
+min2
+sleep 1
+maj2
+sleep 1
+min3
+sleep 1
+maj3
+sleep 1
+perfect4
+sleep 1
+aug4
+sleep 1
+perfect5
+sleep 1
+min6
+sleep 1
+maj6
+sleep 1
+min7
+sleep 1
+maj7
+sleep 1
+octave
+sleep 1
