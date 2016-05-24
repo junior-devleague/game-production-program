5746ebbf51132a3c0b207bbaab2214ad5b4d9c2c
Aisis <aisis@users.noreply.github.com>
2016-05-23 15:19:58 -1000

    Create sample.rb

---
+++ GameMusicComposition/Week3/sample.rb
@@ -0,0 +1,3 @@
+##This is pulling the "sample" from Sonic-Pi's software.
+##sample "/Applications/Sonic Pi.app/etc/samples", "drum_bass_soft.flac"
+sample "/Users/aisis/Desktop/SP/samples/violinc4.wav" #play your own sound file.
