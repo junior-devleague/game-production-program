d828de80cf0bc4fec98fc10b8e45d58d21575018
Charles Nguyen <charles@devleague.com>
2016-05-28 06:54:42 -1000

    sound speed
    added an engine to be able to change the sound speed of any music.

--- GameDevelopment/Week4/music-jump/js/main.js
+++ GameDevelopment/Week4/music-jump/js/main.js
@@ -1,3 +1,75 @@
+  var WarpedSound = function(game, key, volume, loop) {
+    Phaser.Sound.call(this, game, key, volume, loop);
+    game.sound._sounds.push(this);
+  }
+
+  WarpedSound.prototype = Object.create(Phaser.Sound.prototype);
+
+  WarpedSound.prototype.constructor = WarpedSound;
+
+  WarpedSound.prototype.setSpeed = function(speed) {
+    if(!!this._sound) {
+      this._sound.playbackRate.value = speed;
+    } else {
+      this.onPlay.addOnce(function() {
+        this._sound.playbackRate.value = speed;
+      }, this);
+    }
+  }
+
+  WarpedSound.prototype.tweenSpeed = function(speed, duration) {
+    function applyTween(s, d) {
+      game.add.tween(this._sound.playbackRate).to({value: s}, d, Phaser.Easing.Linear.NONE, true);
+    }
+    if(!!this._sound) {
+      applyTween.call(this, speed, duration);
+    } else {
+      this.onPlay.addOnce(function() {
+         applyTween.call(this, speed, duration)
+      }, this);
+    }
+  }
+
+  WarpedSound.prototype.createFilter = function(maxHZ) {
+    maxHZ = maxHZ || 440;
+    function applyFilter(hz) {
+      this._filter = this.context.createBiquadFilter();
+      // Create the audio graph.
+      this._sound.connect(this._filter);
+      this._filter.connect(this.context.destination);
+      // Create and specify parameters for the low-pass filter.
+      this._filter.type = 0; // Low-pass filter. See BiquadFilterNode docs
+      this._filter.frequency.value = hz; // Set cutoff to passed in HZ
+    };
+
+    if(!!this._sound) {
+      applyFilter.call(this, maxHZ);
+    } else {
+      this.onPlay.addOnce(function() {
+         applyFilter.call(this, maxHZ)
+      }, this);
+    }
+  };
+
+  WarpedSound.prototype.tweenFilter = function(maxHZ, duration) {
+    maxHZ = maxHZ || 100;
+    duration = duration || 1000;
+    function applyTween(s, d) {
+      if(!this._filter) {
+        this.createFilter();
+      }
+      game.add.tween(this._filter.frequency).to({value: s}, d, Phaser.Easing.Linear.NONE, true);
+    }
+    if(!!this._sound) {
+      applyTween.call(this, maxHZ, duration);
+    } else {
+      this.onPlay.addOnce(function() {
+         applyTween.call(this, maxHZ, duration)
+      }, this);
+    }
+  }
+var audio;
+
 //This sets the variable for the spacebar.
 var spaceKey;
 //This sets the variable for music
@@ -40,13 +112,11 @@ var mainState = {
 		game.renderer.renderSession.roundPixels = true;
 
 		// This would be a good place to start the general music for the game.
+		audio = new WarpedSound(game, 'music', 0.3);
 		gameSound = game.add.audio('win');
 		coinSound = game.add.audio('coin');
-		gameMusic = game.add.audio('music');
-		//gameMusic = a Phaser.Sound objectvar pbr =  { value: 2, defaultValue: 2};
-		gameMusic.volume -= 0.5;
-		gameMusic._sound.playbackRate.value = pbr;
-		gameMusic.play();
+    audio.tweenSpeed(1.5);
+		audio.play();
 
 		//This sets up a group call platforms. For future functionality we can set all horizontal surfaces to this group.
 		platforms = game.add.group();
@@ -163,7 +233,7 @@ var mainState = {
 		//This will tell you "You Lose!" if the player is pushed off the left side of the screen.
 		function gameOver(player,obstacle){
 			gameOverText = game.add.text(350,200, 'You Lose!', {fill: '#ff0000'});
-			gameMusic.pause();
+			audio.pause();
 			gameSound.play();
 			player.kill();
 			obstacle.kill();
