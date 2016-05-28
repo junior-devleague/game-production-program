c1948519acc4272e0c151b0a1921cb2fe3836b5d
coolernow <coolernow641@gmail.com>
2016-05-28 10:23:50 -1000

    Create Blast pi.rb
    I made a thing, it's good (maybe.)

---
+++ GameMusicComposition/Week4/JD/Blast pi.rb
@@ -0,0 +1,46 @@
+loop do
+  use_bpm 135
+  use_synth :dsaw
+  play :a3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.125
+  sleep 1.8
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.125
+  sleep 1.8
+  play :a3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.125
+  sleep 1.8
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.125
+  sleep 0.9
+  play :d4, sustain: 0.375
+  sleep 1.0
+  play :a3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.125
+  sleep 1.8
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.375
+  sleep 0.65
+  play :fs3, sustain: 0.125
+  sleep 0.9
+  play :e4, sustain: 0.375
+  sleep 0.69
+  play :fs4, susrain: 0.4
+  sleep 0.9
+end
