d9a8dfd5231dd044150660561ffd90d96a9ba528
Aisis <aisis@users.noreply.github.com>
2016-05-27 18:51:23 -1000

    Update melody-filled-bass.rb
    Flows better

--- GameMusicComposition/Week4/Aisis/melody-filled-bass.rb
+++ GameMusicComposition/Week4/Aisis/melody-filled-bass.rb
@@ -3,14 +3,14 @@ loop do
     2.times do
       sample :vinyl_hiss
       use_synth :piano
-      play_pattern_timed [60,67,65,60,64,64,65],[0.4], decay: 0.5, sustain: 0.5
+      play_pattern_timed [60,67,65,60,64,64,65],[0.4], decay: 0.5, sustain: 0.6
       sample :bd_klub
       
     end
     
     1.times do
       sample :ambi_glass_hum, pitch:rand(1)
-      play_pattern_timed [62,69,67,62,65,65,67,65,64],[0.4], decay: 0.5, sustain: 0.5
+      play_pattern_timed [62,69,67,62,65,65,67,65,64],[0.4], decay: 0.5, sustain: 0.6
       sample :bd_boom
     end
   end
