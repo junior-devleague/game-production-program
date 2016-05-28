f6b7c7864ecdbdccfe92e23ca9e0aa500fb1ff27
Aisis <aisis@users.noreply.github.com>
2016-05-27 18:40:47 -1000

    atmosphere added
    I added room tone to it. How did I do that?

---
+++ GameMusicComposition/Week4/Aisis/melody-filled.rb
@@ -0,0 +1,15 @@
+loop do
+  with_fx :reverb do
+    2.times do
+      sample :vinyl_hiss
+      use_synth :piano
+      play_pattern_timed [60,67,65,60,64,64,65],[0.4], decay: 0.5, sustain: 0.5
+      
+    end
+    
+    1.times do
+      sample :ambi_glass_hum, pitch:rand(1)
+      play_pattern_timed [62,69,67,62,65,65,67,65,64],[0.4], decay: 0.5, sustain: 0.5
+    end
+  end
+end
